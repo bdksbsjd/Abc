23d70caccb48eca3b1520aca56f42d4f3c90e3c3
bdksbsjd <bgmigamers82691@gmail.com>
2025-01-11 21:12:41 +0530

    Update sasuke.py

--- sasuke.py
+++ sasuke.py
@@ -11,7 +11,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7396072961:AAHB67mVvtNGaNbD0jk2yZjMi7yGZujZdk8')
+bot = telebot.TeleBot('7550194604:AAEQTlAMfVfLumFVa17Vy29sRLrNr8a7Xp8')
 
 # Admin user IDs
 admin_id = ["1713986516"]
